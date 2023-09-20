0a5f6cc730beab68aa5de17d387acacede0b9f1e
Aleksei Gubin <a.v.gubin0911@gmail.com>
2023-09-20 15:38:54 +0600

    initial commit

---
+++ helper.go
@@ -0,0 +1,63 @@
+
+
+package uiconnector
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/foliagecp/easyjson"
+	sfplugins "github.com/foliagecp/sdk/statefun/plugins"
+)
+
+func createLink(ctx *sfplugins.StatefunContextProcessor, from, to, linkType string, body *easyjson.JSON, tags ...string) error {
+	const op = "functions.graph.ll.api.link.create"
+
+	link := easyjson.NewJSONObject()
+	link.SetByPath("descendant_uuid", easyjson.NewJSON(to))
+	link.SetByPath("link_type", easyjson.NewJSON(linkType))
+
+	if body == nil {
+		link.SetByPath("link_body", easyjson.NewJSONObject())
+	} else {
+		link.SetByPath("link_body", *body)
+	}
+
+	if len(tags) > 0 {
+		link.SetByPath("link_body.tags", easyjson.JSONFromArray(tags))
+	}
+
+	if _, err := ctx.GolangCallSync(op, from, &link, nil); err != nil {
+		return fmt.Errorf("create link: %w", err)
+	}
+
+	return nil
+}
+
+func getChildrenUUIDSByLinkType(ctx *sfplugins.StatefunContextProcessor, uuid, filterLinkType string) []string {
+	result := make([]string, 0)
+
+	pattern := uuid + ".out.ltp_oid-bdy.>"
+	if filterLinkType != "" {
+		pattern = uuid + ".out.ltp_oid-bdy." + filterLinkType + ".>"
+	}
+
+	for _, key := range ctx.GlobalCache.GetKeysByPattern(pattern) {
+		split := strings.Split(key, ".")
+		if len(split) == 0 {
+			continue
+		}
+
+		lastkey := split[len(split)-1]
+		result = append(result, lastkey)
+	}
+
+	sort.Strings(result)
+
+	return result
+}
+
+func generateSessionID(id string) string {
+	return "session_client_" + id
+}
---
+++ statefun.go
@@ -0,0 +1,63 @@
+
+
+package uiconnector
+
+import (
+	"github.com/foliagecp/sdk/statefun"
+)
+
+const (
+	clientIngressFunction = "ui.ingress"
+	clientEgressFunction  = "ui.pre.egress"
+
+	sessionInitFunction          = "functions.client.session.init"
+	sessionCreateFunction        = "functions.client.session.create"
+	sessionDeleteFunction        = "functions.client.session.delete"
+	sessionUnsubFunction         = "functions.client.session.unsub"
+	sessionCommandFunction       = "functions.client.session.command"
+	sessionAutoControlFunction   = "functions.client.session.auto.control"
+	clientControllersSetFunction = "functions.client.controllers.set"
+
+	controllerSetupFunction   = "functions.controller.setup"
+	controllerUnsubFunction   = "functions.controller.unsub"
+	controllerConstructCreate = "functions.controller.construct.create"
+
+	triggerCreateFunction           = "functions.trigger.create"
+	triggerSubscriberUpdateFunction = "functions.trigger.subscriber.update"
+	triggerUpdateFunction           = "functions.trigger.update"
+)
+
+type statefunHandler struct {
+	runtime *statefun.Runtime
+	cfg     *config
+}
+
+func RegisterAllFunctionTypes(runtime *statefun.Runtime, opts ...UIOpt) {
+	h := &statefunHandler{
+		runtime: runtime,
+		cfg:     defaultConfig,
+	}
+
+	for _, opt := range opts {
+		opt(h)
+	}
+
+	statefun.NewFunctionType(runtime, h.cfg.IngressTopic, h.clientInit, *statefun.NewFunctionTypeConfig())
+	statefun.NewFunctionType(runtime, clientEgressFunction, h.clientEgress, *statefun.NewFunctionTypeConfig())
+
+	statefun.NewFunctionType(runtime, sessionInitFunction, h.sessionInit, *statefun.NewFunctionTypeConfig())
+	statefun.NewFunctionType(runtime, sessionCreateFunction, h.createSessionFunction, *statefun.NewFunctionTypeConfig())
+	statefun.NewFunctionType(runtime, sessionDeleteFunction, h.deleteSessionFunction, *statefun.NewFunctionTypeConfig())
+	statefun.NewFunctionType(runtime, sessionUnsubFunction, h.unsubSessionFunction, *statefun.NewFunctionTypeConfig())
+	statefun.NewFunctionType(runtime, sessionAutoControlFunction, h.clientSessionAutoControl, *statefun.NewFunctionTypeConfig())
+	statefun.NewFunctionType(runtime, sessionCommandFunction, h.sessionCommand, *statefun.NewFunctionTypeConfig())
+	statefun.NewFunctionType(runtime, clientControllersSetFunction, h.clientControllersSet, *statefun.NewFunctionTypeConfig())
+
+	statefun.NewFunctionType(runtime, controllerSetupFunction, h.setupControllerFunction, *statefun.NewFunctionTypeConfig())
+	statefun.NewFunctionType(runtime, controllerUnsubFunction, h.unsubControllerFunction, *statefun.NewFunctionTypeConfig())
+	statefun.NewFunctionType(runtime, controllerConstructCreate, h.createControllerConstruct, *statefun.NewFunctionTypeConfig())
+
+	statefun.NewFunctionType(runtime, triggerCreateFunction, h.createTriggerFunction, *statefun.NewFunctionTypeConfig())
+	statefun.NewFunctionType(runtime, triggerSubscriberUpdateFunction, h.updateTriggerSubscriber, *statefun.NewFunctionTypeConfig())
+	statefun.NewFunctionType(runtime, triggerUpdateFunction, h.updateTrigger, *statefun.NewFunctionTypeConfig())
+}

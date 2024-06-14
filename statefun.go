8c730fd5991ef5a1fd2a69e475c270f02ed2d5b6
Andrei Shipov <andreishipov@hotmail.com>
2024-06-15 00:11:22 +0900

    updates: max sessions count changed to 20

--- statefun.go
+++ statefun.go
@@ -21,7 +21,7 @@ import (
 const (
 	sessionsWatchTimeout     = 60 * time.Second
 	sessionInactivityTimeout = 12 * time.Hour
-	maxSessionsCount         = 6
+	maxSessionsCount         = 20
 )
 
 func sessionsKeeper(runtime *statefun.Runtime) {

59fd79d317015b6a83766dd8ca26307ea04fda5c
Andrei Shipov <andreishipov@hotmail.com>
2024-06-27 19:44:19 +0900

    updates: controller update removed; controller object sends updates

--- adapter/decorators/statefun.go
+++ adapter/decorators/statefun.go
@@ -10,7 +10,6 @@ import (
 func Register(r *statefun.Runtime) {
 	statefun.NewFunctionType(r, inStatefun.TYPES_NAVIGATION_DECORATOR, typesNavigation, *statefun.NewFunctionTypeConfig().SetAllowedRequestProviders(sf.AutoRequestSelect).SetMaxIdHandlers(-1))
 	statefun.NewFunctionType(r, inStatefun.IO_LINK_TYPES_DECORATOR, inOutLinkTypes, *statefun.NewFunctionTypeConfig().SetAllowedRequestProviders(sf.AutoRequestSelect).SetMaxIdHandlers(-1))
-	statefun.NewFunctionType(r, inStatefun.CHILDREN_LINK_TYPE_DECORATOR, childrenUUIDsByLinkType, *statefun.NewFunctionTypeConfig().SetAllowedRequestProviders(sf.AutoRequestSelect).SetMaxIdHandlers(-1))
 	statefun.NewFunctionType(r, inStatefun.LINKS_TYPE_DECORATOR, linksByType, *statefun.NewFunctionTypeConfig().SetAllowedRequestProviders(sf.AutoRequestSelect).SetMaxIdHandlers(-1))
 }
 

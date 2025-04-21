b01677e718683d88c62963d8754a8590a464d58e
Andrei Shipov <andreishipov@hotmail.com>
2025-04-22 01:04:14 +0900

    update: CONTROLLER_OBJECT_UPDATE  AutoRequestSelect

--- adapter/statefun.go
+++ adapter/statefun.go
@@ -235,7 +235,7 @@ func StartController(_ sfplugins.StatefunExecutor, ctx *sfplugins.StatefunContex
 
 		// send to update сontroller object
 		payload := easyjson.NewJSONObjectWithKeyValue("force_update_session_id", easyjson.NewJSON(sessionId))
-		ctx.Request(sfplugins.GolangLocalRequest, inStatefun.CONTROLLER_OBJECT_UPDATE, controllerObjectID, &payload, nil)
+		ctx.Request(sfplugins.AutoRequestSelect, inStatefun.CONTROLLER_OBJECT_UPDATE, controllerObjectID, &payload, nil) // Sync call for Golang direct call if possible (speedup?)
 		//ctx.Signal(sfplugins.AutoSignalSelect, inStatefun.CONTROLLER_OBJECT_UPDATE, controllerObjectID, &payload, nil)
 	}
 }
